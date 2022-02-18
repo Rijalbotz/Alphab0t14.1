1535d642bd3f23c1fe6e1337262783b380d0e74c
Rijalbotz <99317707+Rijalbotz@users.noreply.github.com>
2022-02-18 14:34:18 +0700

    Update sewa.js

--- shop/sewa.js
+++ shop/sewa.js
@@ -2,13 +2,29 @@ const pc_sewa = () => {
 return `「 *PRICE LIST* 」
 							
 ⬣ PRICE SEWABOT
+*•PRICE SEWA BOT BY RIJAL JB*
 
-5k 2 minggu
-10k 1 bulan+
+*FITUR NYA GG PASTINYA ON 24 JAM KECUALI DELAY/PAKET HABIS*
 
-12 1 bulan+Prem
+*FITUR :*
+*ANTIVIRTEX ON*
+*WELCOME OTOMATIS*
+*NSFW-HENTAI Dll ON*
+*ADA FITUR DOWNLOAD TT.IG.FB.YT*
+*Dll Fitur-fitur Menariknya*
 
-15 Manent+Prem
+*HARGA SEWA BOT :*
+*5K 7 Day*
+*~7K 7 Day+Prem*
+*10k 30 Day*
+*~12k 30 Day+Prem*
+*[SPESIAL]15k Manen+Prem*
+
+*wa.me/+6283159252524?text=Assalamu'alaikum+Bang+Mau+Sewa+BOT*
+
+*Gunakan BOT DENGAN BIJAK 👍🏼*
+
+*LINK GC :https://chat.whatsapp.com/GNRIi7Cfp8hGNsTlAEiuUD*
 
 
 ⬣ MINAT? PM
